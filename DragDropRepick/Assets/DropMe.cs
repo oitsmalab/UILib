d79b0be85a1fcc938f8dfe3c1977aa6dfac7ee18
yanoty3d <vzb10326@me.com>
2014-08-30 23:13:57 +0900

    dragObjectを追加。これがDropされるべきGameObjectを示す。 OnDropで、dragObjectをdragIconにInstantiate, dragIconをCanvas内で一番下の子供にする。 そこで使うFindInParentsメソッドを追加。

--- DragDropRepick/Assets/DropMe.cs
+++ DragDropRepick/Assets/DropMe.cs
@@ -5,59 +5,82 @@ using UnityEngine.UI;
 
 public class DropMe : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
-	public Image containerImage;
-	public Image receivingImage;
-	private Color normalColor;
-	public Color highlightColor = Color.yellow;
-	
-	public void OnEnable ()
-	{
-				Debug.Log ("OnEnable");
-		if (containerImage != null)
-			normalColor = containerImage.color;
-	}
-	
-	public void OnDrop(PointerEventData data)
-	{
-		containerImage.color = normalColor;
-		
-		if (receivingImage == null)
-			return;
-		
-		Sprite dropSprite = GetDropSprite (data);
-		if (dropSprite != null)
-			receivingImage.overrideSprite = dropSprite;
-	}
+		public Image containerImage;
+		public Image receivingImage;
+		private Color normalColor;
+		public Color highlightColor = Color.yellow;
+		public GameObject dragObject;//DropされるべきGameObject
 
-	public void OnPointerEnter(PointerEventData data)
-	{
-				Debug.Log ("OnPointerEnter");
-		if (containerImage == null)
-			return;
+		public void OnEnable ()
+		{
+			if (containerImage != null)
+				normalColor = containerImage.color;
+		}
 		
-		Sprite dropSprite = GetDropSprite (data);
-		if (dropSprite != null)
-			containerImage.color = highlightColor;
-	}
+		public void OnDrop(PointerEventData data)
+		{
+			containerImage.color = normalColor;
+			
+			if (receivingImage == null)
+				return;
 
-	public void OnPointerExit(PointerEventData data)
-	{
-		if (containerImage == null)
-			return;
-		
-		containerImage.color = normalColor;
-	}
-	
-	private Sprite GetDropSprite(PointerEventData data)
-	{
-		var originalObj = data.pointerDrag;
-		if (originalObj == null)
-			return null;
+			GameObject dragIcon = Instantiate (dragObject) as GameObject;
 
-		var srcImage = originalObj.GetComponent<Image>();
-		if (srcImage == null)
-			return null;
+			var canvas = FindInParents<Canvas>(gameObject);
+
+			dragIcon.transform.SetParent (canvas.transform, false);
+			dragIcon.transform.SetAsLastSibling();
+			dragIcon.GetComponent<RectTransform> ().position = GetComponent<RectTransform> ().position;
+			
+		}
+
+		public void OnPointerEnter(PointerEventData data)
+		{
+					Debug.Log ("OnPointerEnter");
+			if (containerImage == null)
+				return;
+			
+			Sprite dropSprite = GetDropSprite (data);
+			if (dropSprite != null)
+				containerImage.color = highlightColor;
+		}
+
+		public void OnPointerExit(PointerEventData data)
+		{
+			if (containerImage == null)
+				return;
+			
+			containerImage.color = normalColor;
+		}
 		
-		return srcImage.sprite;
-	}
+		private Sprite GetDropSprite(PointerEventData data)
+		{
+			var originalObj = data.pointerDrag;
+			if (originalObj == null)
+				return null;
+
+			var srcImage = originalObj.GetComponent<Image>();
+			if (srcImage == null)
+				return null;
+			
+			return srcImage.sprite;
+		}
+				
+		static public T FindInParents<T>(GameObject go) where T : Component
+		//GameObjectの親であり、型がTであるものを探しにいく
+		{
+				if (go == null) return null;
+				var comp = go.GetComponent<T>();
+
+				if (comp != null)
+						return comp;
+
+				Transform t = go.transform.parent;
+				while (t != null && comp == null)
+				{
+						comp = t.gameObject.GetComponent<T>();
+						t = t.parent;
+				}
+				return comp;
+		}
 }
