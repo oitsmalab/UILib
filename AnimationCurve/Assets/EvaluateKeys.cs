2b40ef65ff34c453fce79624b87a3ec6ac5bf4c1
yanoty3d <vzb10326@me.com>
2014-11-03 18:36:59 +0900

    空のオブジェクトを使ってアニメーションの場所を変更させるデモ。

---
+++ AnimationCurve/Assets/EvaluateKeys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EvaluateKeys : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+		Animation anim = GetComponent<Animation> ();
+		anim.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
