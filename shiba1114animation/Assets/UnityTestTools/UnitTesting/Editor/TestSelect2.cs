f4c2e64e5c0340a0883f046be9e9297950501f23
yanoty3d <vzb10326@me.com>
2014-11-26 16:03:41 +0900

    TestGetRelativeHeightsFromAnimationCurvePatternを小変更し、最初と最後は固定で、真ん中のキーフレームを変えた場合のテストにした。

--- shiba1114animation/Assets/UnityTestTools/UnitTesting/Editor/TestSelect2.cs
+++ shiba1114animation/Assets/UnityTestTools/UnitTesting/Editor/TestSelect2.cs
@@ -4,18 +4,17 @@ using System.Collections;
 
 class TestSelect2
 {
-	[TestCase(1, 1)]
+	[TestCase(0.5f, 2f)]
 	public void TestGetRelativeHeightsFromAnimationCurvePattern1 (float time, float value)
 	{
-
 		AnimationCurve[] animationCurvePattern = new AnimationCurve[1];
-		animationCurvePattern[0] =  new AnimationCurve(new Keyframe(0, 0), new Keyframe(time, value));
-
+		animationCurvePattern[0] =  new AnimationCurve(new Keyframe(0, 0), new Keyframe(time, value), new Keyframe(1f, 1f));
+		
 		select2 select2Ref = new select2 ();
 		select2.Curve[] curveArray = new select2.Curve[1];
-
+		
 		select2Ref.GetRelativeHeightsFromAnimationCurvePattern (animationCurvePattern, curveArray);
-
+		
 		Assert.AreEqual(curveArray[0].height, 1);
 	}
 
