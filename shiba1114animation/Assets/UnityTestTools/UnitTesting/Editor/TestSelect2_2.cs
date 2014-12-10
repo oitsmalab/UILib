bbf8b7f5750b99b0a8e2907cd339a3771b10b089
yanoty3d <vzb10326@me.com>
2014-12-10 23:54:37 +0900

    柴くん用にTestSelect2.cs,田中くん用にTestSelect2_2.csを作成。

---
+++ shiba1114animation/Assets/UnityTestTools/UnitTesting/Editor/TestSelect2_2.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections;
+
+public class TestSelect2_2 : MonoBehaviour
+{
+		//GetRelativeHeightsFromAnimationCurvePattern　テスト
+	//********************************************************************************************************************************
+	struct curve{
+		public float height;
+		public float[] keyframeV;
+	}
+
+
+	[TestCase(2f,0,0,1,0)]
+	public void TestGetAdjustAnimationCurve(float targetposition,float transposition,AnimationCurve[] animationCurvePattern ,int animationNumber,Curve[] curveArray){
+
+		float[] x = { 3f,2f };
+
+		AnimationCurve curve = new AnimationCurve ();
+
+		select2 select2Ref = new select2 ();
+
+	 	curve = select2Ref.GetAdjustedAnimationCurve(targetposition,transposition,animationCurvePattern,animationNumber,curveArray);
+
+		Assert.AreEqual(curve,x);
+	}
+}
